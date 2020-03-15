7523aec401826f59e725b1b42f39568b63219be1
Marco <23193188+Marco1097@users.noreply.github.com>
2020-03-15 15:33:12 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,20 +14,22 @@ async def on_ready():
     for guild in bot.guilds:
         if guild.name == GUILD:
             break
-    print("OK\n\n")
+    print("OK", file=open("log.txt", "a"))
 
 @bot.event
 async def on_voice_state_update(member, before, after):
     if(before.channel == None):
+        print(datetime.datetime.now(), "|", member, "| CONNECTED |", str(after.channel), file=open("log.txt", "a"))
         print(datetime.datetime.now(), "|", member, "| CONNECTED |", str(after.channel))
     elif(after.channel == None):
+        print(datetime.datetime.now(), "|", member, "| DISCONNECTED |", str(before.channel), file=open("log.txt", "a"))
         print(datetime.datetime.now(), "|", member, "| DISCONNECTED |", str(before.channel))
     elif(before.channel != after.channel):
-        print(datetime.datetime.now(), "|", member, "| MOVED |", str(before.channel), "->", str(after.channel), "| Muted:", str(after.self_mute).replace("True", "yes").replace("False", "no"))
+        print(datetime.datetime.now(), "|", member, "| MOVED |", str(before.channel), "->", str(after.channel), file=open("log.txt", "a"))
+        print(datetime.datetime.now(), "|", member, "| MOVED |", str(before.channel), "->", str(after.channel))
     elif(before.self_stream != after.self_stream):
+        print(datetime.datetime.now(), "|", member, "| STREAMING |", str(after.self_stream).replace("True", "ON").replace("False", "OFF"), file=open("log.txt", "a"))
         print(datetime.datetime.now(), "|", member, "| STREAMING |", str(after.self_stream).replace("True", "ON").replace("False", "OFF"))
-    else:
-        print(datetime.datetime.now(), "Error!!")
 
-print("Starting...")
+print("Starting...", file=open("log.txt", "a"))
 bot.run(TOKEN)
